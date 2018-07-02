8506c0b7af5788b81573c95d5a742dff7b8ffafe
VladPalamarchuk88 <vladpalamarchyk6788@gmail.com>
2018-07-02 20:31:31 +0300

    logig change3

--- src/main/java/ua/kiev/prog/automation/framework/App.java
+++ src/main/java/ua/kiev/prog/automation/framework/App.java
@@ -1,15 +1,24 @@
 package ua.kiev.prog.automation.framework;
 
 import ua.kiev.prog.automation.framework.product.app.ProgKievUa;
+
 public class App
 {
+    static private boolean _standalone;
+
+    static public boolean isStandalone()
+    {
+        return _standalone;
+    }
+
     static public void main(String[] args)
     {
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+        if (args[0].equals("standalone"))
+            _standalone = true;
 
-        ProgKievUa product = new ProgKievUa();
+        System.setProperty ("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+        ProgKievUa product = new ProgKievUa ();
         product.forum().setURL("https://prog.kiev.ua/forum");
         product.runTests();
-
     }
 }
